a2affa513582287dafe6e6d0d8f9531962a56049
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-01-19 16:17:56 +0530

    dynamic data for signup api

--- src/app.js
+++ src/app.js
@@ -4,17 +4,13 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Akshay",
-    lastName: "Saini",
-    emailId: "akshay@asini.com",
-    password: "as@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
-
     res.send("User added successfully");
   } catch (err) {
     console.error("error saving user", err);
